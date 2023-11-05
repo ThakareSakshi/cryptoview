3b0e9ddd0b1552b274b4247d5b10ead2f7ab443f
ThakareSakshi <sakshithakare121@gmail.com>
2023-11-05 23:51:55 +0530

    added

--- search.js
+++ search.js
@@ -23,12 +23,20 @@ function displayListOFCoins(coinList){
         let eachCoin=document.createElement("div");
         eachCoin.classList.add("list-item");
         eachCoin.innerHTML=`
-        <div class="name_container"><img src="${element.thumb}"><span class="name" > ${element.name} ${element.symbol}</span></div> <button>more info</button>`
+        <div class="name_container"><img src="${element.thumb}"><span class="name" > ${element.name} ${element.symbol}</span></div> <button onclick='moreInfo("${element.id}")'>more info</button>`
          coin_list.appendChild(eachCoin);
     });
 
 }
 
-function moreInfo(){
-    
+async function moreInfo(coinname){
+let coin_info=await fetch(`https://api.coingecko.com/api/v3/coins/${coinname}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`);
+let result=await coin_info.json();
+console.log(result);
+document.querySelector("main").style.display="none";
+document.querySelector("#more_info").style.display="unset"
+document.querySelector("#more_info").innerHTML=`
+<h1>${result.name}</h1>
+<div><img src="${result.image.large}"></div>
+<div><h2>Description:</h2><p>${result.description.en}<p><div>`
 }
